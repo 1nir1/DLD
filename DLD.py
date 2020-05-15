af8e90784c8a9eb6c52d7c4303650076df7c40f8
nir <nir.kravetzky@tipalti.com>
2020-05-15 15:48:59 +0300

    add deltaX factor to commandline params

--- DLD.py
+++ DLD.py
@@ -7,7 +7,7 @@ from Models.Line import Line
 from Models.DataExtractor import ExtractPointsByArea
 from Models.IO import GetCommandLineParams
 
-(fileName, minArea, maxArea) = GetCommandLineParams()
+(fileName, minArea, maxArea, deltaXFactor) = GetCommandLineParams()
 
 points = ExtractPointsByArea(fileName, minArea, maxArea)
 points.sort(key=lambda point: point._x)
@@ -17,39 +17,32 @@ lines = []
 
 for point in points:
     validLines = [line for line in lines if line.CanPointBeAdded(point)]
-
     lineProximities = {line.GetPointProximityValue(point): line for line in validLines}
-    print("lineProximities {0}".format(lineProximities))
         
     if len(lineProximities) > 0:
         firstKey = sorted(lineProximities.keys())[0]
         bestLine = lineProximities[firstKey]
         bestLine.AddPoint(point)
-        # INSTEAD - get result from line - is the point ok to be added?
-        # if so, add line to array of candidates, don't break the check - continue adding lines to that array
     else:   
-        # change check to - "is array of fit lines empty"
         line = Line()
         line.AddPoint(point)
         lines.append(line)
-        #if added here - break
     
-    # add test here - which line from the array is the fittest - add the point to it
-
 filteredLines = [line for line in lines if line.Size() > 1]
 with open('out.txt', 'w') as f:
     for line in filteredLines:
         (xCoordinates, yCoordinates) = line.GetCoordiantes()
-        x = np.asarray(xCoordinates) 
+        x = np.asarray(xCoordinates)
         y = np.asarray(yCoordinates)
         b, m = polyfit(x, y, 1)
         yValuesAfterPolyfit = b + m * x
         lineLength = hypot(x[-1] - x[0], yValuesAfterPolyfit[-1] - yValuesAfterPolyfit[0])
-        if lineLength < 0.7 * deltaX:
+        if lineLength < deltaXFactor * deltaX:
             continue
+
         plt.plot(x, y)
         plt.plot(x, yValuesAfterPolyfit, '-')
-        print("new line, average radius: {0}".format(line.GetAverageRadius()), file=f)
+        print("new line, average radius: {0}, biggestYPoint {1}, smallestYPoint {2}".format(line.GetAverageRadius(), line._biggestYPoint, line._smalletYPoint), file=f)
         print(line._points, file=f)
         # linearLine = Line(x, yValuesAfterPolyfit)
         dictionary = dict(zip(x, yValuesAfterPolyfit))
--- Models/IO.py
+++ Models/IO.py
@@ -5,10 +5,13 @@ def GetCommandLineParams():
     parser.add_argument('--fileName', nargs=1, type=str, default=None, help='The path for the csv source file')
     parser.add_argument('--minArea', type=float, default=0, help='Minimum area for circles - smaller circles will be filtered out. Default value is 0')
     parser.add_argument('--maxArea', type=float, default=float("inf"), help='Maximum area for circles - bigger circles will be filtered out. Default value is infinity')
+    parser.add_argument('--deltaXFactor', type=float, default=0.7, help='Factor to multiply the deltaX of the sample with - lines below that deltaX * factor length, will be filtered out')
     args=parser.parse_args()
 
-    minArea = args.minArea
-    maxArea = args.maxArea
     fileName = args.fileName
     fileName = fileName[0].replace("'","")
-    return (fileName, minArea, maxArea)
+    minArea = args.minArea
+    maxArea = args.maxArea
+    deltaXFactor = args.deltaXFactor
+
+    return (fileName, minArea, maxArea, deltaXFactor)
