1da5599a20cec1dfe2c86ed1fa58e9422caa4e13
nir <nir.kravetzky@tipalti.com>
2020-05-15 17:14:12 +0300

    add equality to point

--- Models/Point.py
+++ Models/Point.py
@@ -4,8 +4,16 @@ class Point:
         self._y = float(y)
         self._radius = float(feret) / 2
 
+    def __eq__(self, other): 
+        if not isinstance(other, Point):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self._x == other._x and self._y == other._y and self._radius == other._radius
+
     def __str__(self):
         return "({0},{1}), {2}".format(self._x, self._y, self._radius)
 
     def __repr__(self):
         return str(self)
+    
