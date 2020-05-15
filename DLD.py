3d755eff5fa935de8b21e1cdd5738dd227e0a804
nir <nir.kravetzky@tipalti.com>
2020-05-15 15:28:25 +0300

    add support for commandline params

--- DLD.py
+++ DLD.py
@@ -1,18 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+
 from numpy.polynomial.polynomial import polyfit
 from math import hypot
 from Models.Point import Point
 from Models.Line import Line
 from Models.DataExtractor import ExtractPointsByArea
 
-# TODO CONFIGURABLE BY USER #
-minArea = 1
-maxArea = 15
-file = 'Results.csv'
-#############################
+def GetCommandLineParams():
+    parser=argparse.ArgumentParser(description='DewettingLineDetector')
+    parser.add_argument('--fileName', nargs=1, type=str, default=None, help='The path for the csv source file')
+    parser.add_argument('--minArea', type=float, default=0, help='Minimum area for circles - smaller circles will be filtered out. Default value is 0')
+    parser.add_argument('--maxArea', type=float, default=float("inf"), help='Maximum area for circles - bigger circles will be filtered out. Default value is infinity')
+    args=parser.parse_args()
 
-points = ExtractPointsByArea(file, minArea, maxArea)
+    minArea = args.minArea
+    maxArea = args.maxArea
+    fileName = args.fileName
+    fileName = fileName[0].replace("'","")
+    return (fileName, minArea, maxArea)
+
+(fileName, minArea, maxArea) = GetCommandLineParams()
+
+points = ExtractPointsByArea(fileName, minArea, maxArea)
 points.sort(key=lambda point: point._x)
 deltaX = points[-1]._x - points[0]._x
 
